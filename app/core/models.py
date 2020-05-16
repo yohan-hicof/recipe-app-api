a2e12f1de6c80728239a0c3de1359f310e8a6e4b
yohan <yohan@hicof.com>
2020-05-16 10:38:23 +0200

    Creating the user model, the test going with it

---
+++ app/core/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and saves a new user"""
+        if not email:
+            raise ValueError('User must have a valid email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and save a superuser"""
+        user = self.create_user(email=email,password=password)
+        user.is_staff=True
+        user.is_superuser=True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that support email as username"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+
